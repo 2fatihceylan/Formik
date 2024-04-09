7f30eab7a1c56bfa0f963d3e39b9840d6fee462a
2fatihceylan <49781433+2fatihceylan@users.noreply.github.com>
2024-04-09 17:09:04 +0300

    formik

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import { Formik } from 'formik';
 
 function App() {
+
+
+  const submitHandle = (values) => {
+    alert(JSON.stringify(values));
+    console.log(values.email)
+
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+      
+      <div className='formik-container'>
+      <Formik
+     
+      initialValues={{ email: '', password: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        } else if(values.password.length<4){
+          errors.password = 'Password must be at least 4 character!'
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          //submitHandle(JSON.stringify(values))
+          submitHandle(values)
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form className='form-box' onSubmit={handleSubmit}>
+          <input
+            className='input-field'
+            type="email"
+            name="email"
+            placeholder='Email Address'
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.email && touched.email && errors.email}
+          <input
+            className='input-field'
+            type="password"
+            name="password"
+            placeholder='Password'
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+          />
+          {errors.password && touched.password && errors.password}
+          <button className='button-submit' type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+      </div>
+
+
+  </div>
   );
 }
 
